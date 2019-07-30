78a615617642fcc9d9e3e78d96d93c6375616286
Dragan <dpepic@mail.com>
2019-07-30 17:03:43 +0200

    Platno :)

---
+++ Plate/Plate/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plate
+{
+    class UI
+    {
+        //TODO ovo ide u firmu!!!
+        static List<Radnik> radnici = new List<Radnik>();
+        static void Main(string[] args)
+        {
+            do
+            {
+                
+                Meni();
+                //True kod ReadKey samo znaci da nece ispisivati 
+                //sta smo uneli u konzolu
+                ConsoleKeyInfo dugme = Console.ReadKey(true);
+                if (!(int.TryParse(dugme.KeyChar.ToString(), out int izbor)))
+                    break; //inverzija, ako nije prosao onda idemo na if
+                           //i ispadamo iz petlje
+                           //Samo radi citljivosti je ovako
+                switch(izbor)
+                {
+                    case 1:
+                        Console.WriteLine("Unesite ime i prezime: ");
+                        string[] imeIprezime = Console.ReadLine().Split(' ');
+                        UI.radnici.Add(new Radnik(imeIprezime[0], imeIprezime[1]));
+                        break;
+                    case 3:
+                        foreach (Radnik r in radnici)
+                            Console.WriteLine(r.ToString());
+                        break;
+                }
+
+            } while (true);
+        }
+
+        static void Meni()
+        {
+            Console.WriteLine("1. Unos radnika\n" +
+                              "2. Otpusti radnika\n" +
+                              "3. Pregled radnika\n");
+            Console.WriteLine("karakter - Izlaz iz programa");
+            Console.WriteLine("Izbor: ");
+        }
+    }
+}
---
+++ Plate/Plate/Radnik.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plate
+{
+    class Radnik
+    {
+        //TODO titula
+        internal string Ime { private set; get; }
+        internal string Prezime { private set; get; }
+        internal string PunoIme
+        {
+            get => $"{this.Prezime} {this.Ime}";
+        }
+
+        internal uint neplaceniDani;
+
+        //Celobrojno izrazen procenat, +- na platu
+        internal int modifikator;
+
+        //EXP objasni properties i pozadinska polja
+        //EXP objasni tagove, kada smo kod toga :D
+        private long id;
+        internal long ID
+        {
+          private set => this.id = Radnik.iduciID++;
+          get => this.id;
+        }
+
+        static long iduciID;
+
+        public Radnik(string i, string p)
+        {
+            this.Ime = i;
+            this.Prezime = p;
+            //HACK koristimo property, vrednost se zanemaruje ovde
+            this.ID = 0;
+        }
+
+        /*Lambda izraz, ovo je isto kao i
+         * public override string ToString()
+         * {
+         *      return $"{this.ID} -- {this.PunoIme}";
+         * } */
+        public override string ToString() => $"{this.ID} -- {this.PunoIme}";
+    }
+}
