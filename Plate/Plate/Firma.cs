39bc6c9a943c629a78f4d136d93c9c9d8640cfcd
Dragan <dpepic@mail.com>
2019-08-02 10:37:10 +0200

    Napokon

---
+++ Plate/Plate/Firma.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plate
+{
+    class Firma
+    {
+        static string naziv, JIB, PIB;
+        internal static decimal balans { get; private set; }
+
+        internal static HashSet<Pozicija> pozicije = new HashSet<Pozicija>();
+
+        //TODO TRANSAKCIJE TREBA DA BUDU OBJEKTI
+        internal static void isplata(decimal svota, string firma)
+        {
+
+        }
+        internal static void isplata(Radnik r)
+        {
+
+        }
+        internal static void isplata(Pozicija p)
+        {
+
+        }
+
+        internal static bool radnikPoId(long id, out Radnik radnik)
+        {
+            foreach (Pozicija p in Firma.pozicije)
+                foreach (Radnik r in p.radnici)
+                    if (r.ID == id)
+                    {
+                        radnik = r;
+                        return true;
+                    }
+            radnik = null;
+            return false;
+        }
+    }
+}
