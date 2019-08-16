24473620e3466a7ac31063e72ef2f22cf2c4abb3
Dragan <dpepic@mail.com>
2019-08-16 16:29:38 +0200

    Komentari na CSV

--- InterfaceCSV/InterfaceCSV/Program.cs
+++ InterfaceCSV/InterfaceCSV/Program.cs
@@ -13,10 +13,12 @@ namespace InterfaceCSV
 
         public static void dodajUrecnik(Artikal a, int kol)
         {
+            //TODO ovo je mozda suvisno, testirati da li ce += na neinicijalizovanu vrednost
+            //inicijalizovati int na 0 ili ce da izbije exception
             if (recnik.ContainsKey(a))
-                recnik[a] += kol;
+                recnik[a] += kol; //Ako vec imamo artikal dodajemo kolicinu na postojecu
             else
-                recnik.Add(a, kol);
+                recnik.Add(a, kol); //Ako ga nemamo,koristimo add
         }
 
         static void Main(string[] args)
@@ -27,31 +29,37 @@ namespace InterfaceCSV
 
             prvi.upisiUfajl("artikli.csv");
             drugi.upisiUfajl("artikli.csv");
-            tri.upisiUfajl("artikli.csv");*/
+            tri.upisiUfajl("artikli.csv");*/  //Upis artikala u fajl
 
-            Artikal.ucitajIzFajla("artikli.csv");
+            //HACK ovo treba popraviti
+            while (Artikal.brojac < File.ReadAllLines("artikli.csv").Length) 
+                new Artikal();
+            Artikal.brojac = 0;
+            
 
             foreach (Artikal a in Artikal.sviArtikli)
                 Console.WriteLine(a.naziv);
 
-            /*dodajUrecnik(prvi, 4);
-            dodajUrecnik(drugi, 2);
-            dodajUrecnik(prvi, 2);
-            dodajUrecnik(tri, 3);
+            dodajUrecnik(Artikal.sviArtikli[0], 4); //Priprema kolicina i artikala za jedan racun
+            dodajUrecnik(Artikal.sviArtikli[1], 2);
+            dodajUrecnik(Artikal.sviArtikli[2], 2);
+            dodajUrecnik(Artikal.sviArtikli[0], 3);
 
-            Racun rac = new Racun(recnik);
-
-            Console.WriteLine(rac.ToString()); */
+            Racun rac = new Racun(recnik); 
+            rac.upisiUfajl("racuni.csv");
+            Console.WriteLine(rac.ToString()); 
 
             Console.ReadKey();
         }
     }
 
-    public interface IFileIO
-    {
+    public interface IFileIO //U interfejsu smo napisali stvari koje zelimo da 
+    {                         //da koriste i racun i artikal koji nam inace nemaju nikakvu vezu
         void upisiUfajl(string f);
-        
-        string formatZaFajl { get; }
+        void ucitajIzFajla(string f);
+        string formatZaFajl { get; } //Ne mozemo da imam promenljivu u interfejsu, no property
+                                     //moze zato sto je on svakako set od dve metode,
+                                     //gettera i settera a ne vrednost kao vrednost
     }
 
     class Artikal : IFileIO
@@ -59,34 +67,39 @@ namespace InterfaceCSV
         internal int cena, stanje;
         internal string sifra, naziv;
         internal static List<Artikal> sviArtikli = new List<Artikal>();
+        internal static int brojac; //brojac nam treba za citanja iz fajla
         public string formatZaFajl
-        {
+        {  //Implementacija property-a je jednostavno zgodan format za upis u csv
             get => $"{this.sifra};{this.naziv};{this.stanje};{this.cena}{Environment.NewLine}";
         }
 
         public void upisiUfajl(string fajl)
-        {
+        {   //No big deal :) kako za neki artikal pozovemo upis on svoje podatke doda u fajl
             File.AppendAllText(fajl, this.formatZaFajl);
         }
     
-        public static void ucitajIzFajla(string fajl)
+        public void ucitajIzFajla(string fajl)
         {
-            Artikal.sviArtikli.Clear();
-
-            foreach(String art in File.ReadAllLines(fajl))
-            {
-                string[] artParametri = art.Split(';');
-                string sifra = artParametri[0];
-                string naziv = artParametri[1];
-                int kolicina = int.Parse(artParametri[2]);
-                int cena = int.Parse(artParametri[3]);
-
-                Artikal.sviArtikli.Add(new Artikal(kolicina, cena, sifra, naziv));
-            }
+            //Ucitavamo red iz fajla, tj, ucitavamo sve redove pa uzmemo onaj koji nam 
+            //treba po vrednosti staticnog brojaca i u isto vreme ga inkrementujemo
+            //da drugima damo do znanja sta je njihovo
+                string[] artikli = File.ReadAllLines(fajl);
+                string[] artParametri = artikli[Artikal.brojac++].Split(';');
+                this.sifra = artParametri[0];
+                this.naziv = artParametri[1];
+                this.stanje = int.Parse(artParametri[2]);
+                this.cena = int.Parse(artParametri[3]);   
         }
 
         public Artikal()
-        { }
+        {
+            //Ako pozovemo konstruktor bez parametara ucitavamo sledeci artikal iz
+            //fajla ako takav postoji. Simpaticno, ovo je solidna metoda jer cak i ako
+            //prekinemo ucitavanja iz fajla i dodamo novi artikal pozivajuci drugi konstruktor,
+            //citanje iz fajla se kasnije nastavlja tacno tamo gde je stalo :)
+            this.ucitajIzFajla("artikli.csv");
+            Artikal.sviArtikli.Add(this);
+        }
 
         public Artikal(int c, int s, string sifra, string n)
         {
@@ -94,13 +107,28 @@ namespace InterfaceCSV
             this.stanje = s;
             this.sifra = sifra;
             this.naziv = n;
+            Artikal.sviArtikli.Add(this);
         }
     }
 
-    class Racun
+    class Racun : IFileIO
     {
+        //Unutar racuna imamo recnik koji za kljuc ima artikal a vrednost tog kljuca kolicinu
+        //artikla na racunu
         Dictionary<Artikal, int> artikli;
         int total;
+        public string formatZaFajl
+        {
+            get
+            {
+                string s = "";
+                foreach (Artikal a in this.artikli.Keys)
+                    s += $"{a.sifra};{this.artikli[a]};"; //Grabimo sifru iz objekta artikal
+                s += $"{this.total}{Environment.NewLine}"; //i kolicinu iz recnika po tom kljucu
+                return s;
+            }
+        }
+
 
         public Racun(Dictionary<Artikal, int> a)
         {
@@ -111,6 +139,7 @@ namespace InterfaceCSV
             }
         }
 
+        
         public override string ToString()
         {
             string s = "Stavke:\n";
@@ -119,6 +148,16 @@ namespace InterfaceCSV
             s += "=======================\n";
             s += $"Total: {this.total}";
             return s;
+        }
+
+        public void ucitajIzFajla(string f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void upisiUfajl(string f)
+        {
+            File.AppendAllText(f, this.formatZaFajl);
         }
     }
 }
