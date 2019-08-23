33ed9b22ebe1549f9a515a14033b44e22e6288b0
Dragan <dpepic@mail.com>
2019-08-23 10:27:49 +0200

    WPF binding

---
+++ Binding/Binding/Imena.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding
+{
+    class Imena
+    {
+        public List<string> listaImena
+        {get; set;}
+
+        string naziv;
+
+        public Imena()
+        {
+            this.listaImena = new List<string>();
+        }
+        public string Naziv
+        {
+            get => 
+                this.naziv;
+            private set => 
+                this.naziv = value;
+        } 
+    }
+}
