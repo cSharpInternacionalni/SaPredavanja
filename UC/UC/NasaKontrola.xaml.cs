6f95e7572c57f2de3603591727113ee8c34a6c96
Dragan <dpepic@mail.com>
2019-09-26 17:01:01 +0200

    UC

---
+++ UC/UC/NasaKontrola.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UC
+{
+    /// <summary>
+    /// Interaction logic for NasaKontrola.xaml
+    /// </summary>
+    public partial class NasaKontrola : UserControl
+    {
+        public NasaKontrola()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public string ImeDugmeta { get; set; } = "Dugme";
+    }
+
+    
+}
